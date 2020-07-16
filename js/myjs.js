e99e4245326177b72d8aeb82afb619659c646bf0
frankizrael <frankdelarosacampos@gmail.com>
2020-07-16 18:32:03 -0500

    fixed pedidos

--- js/myjs.js
+++ js/myjs.js
@@ -25,11 +25,16 @@ $('.sub-items-solutions').on('click',function(){
 	let data = $this.attr('data');
 	$('.sub-items-solutions').removeClass('active');
 	$this.addClass('active');
+  $('#frame-solution .swiper-pagination-bullet').eq(data).trigger('click');
 
-	$('.sub-items-solutions').closest('.content-item-solutions').find('.tab-responsive').removeClass('tab-active');
+  $('.content-item-solutions').removeClass('activet');
+  $this.closest('.content-item-solutions').addClass('activet');
+
+	/*
+  $('.sub-items-solutions').closest('.content-item-solutions').find('.tab-responsive').removeClass('tab-active');
 	$this.closest('.content-item-solutions').find('.tab-responsive').addClass('tab-active');
 	$('.solutions-frame').addClass('none');
-	$(data).removeClass('none');
+	$(data).removeClass('none');*/
 });
 
 $('.videoResponsiveJS').on('click',function(){
@@ -37,6 +42,16 @@ $('.videoResponsiveJS').on('click',function(){
 	$this.closest('.tab-responsive').find('.videoResponsive').toggle();
 });
 
+
+$('.img-full').on('click',function(){
+  $('.move-r').addClass('active');
+});
+$('.close-move-r').on('click',function(){
+  $('.move-r').removeClass('active');
+});
+$('.close-modal').on('click',function(){
+  $('.modalShow').hide();
+});
 
 
 const validator = $('#contact').validate({
@@ -72,19 +87,46 @@ const validator = $('#contact').validate({
     let formData = $(form).serialize();
     //url 
     let ajaxUrl = '';
+    $('.modalShow').show();
     //
-    $.ajax({
+    /*$.ajax({
       url: ajaxUrl,
       method: 'POST',
       data: formData
     })
       .done((data, xhrStatus, xhr) => {
        	//openmodal
-
+        $('.modalShow').show();
       })
       .fail(xhr => {
         let err = JSON.parse(xhr.responseText);
         validator.showErrors(err);
-      });
+      });*/
+  }
+});
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  autoplay: {
+    delay: 7000,
+  },
+  on: {
+    slideChangeTransitionEnd: function(){
+      let $bullet = $('#frame-solution .swiper-pagination-bullet');
+      for (let i=0;i<$bullet.length;i++) {    
+        if ($bullet.eq(i).hasClass('swiper-pagination-bullet-active')){
+          $('.sub-items-solutions').removeClass('active');
+          $('.sub-items-solutions').eq(i).addClass('active');
+        }
+      }
+    }
   }
 });
